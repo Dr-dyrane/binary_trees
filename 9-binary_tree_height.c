33109159ef805cddef6956a47c1606f8c2cbdebf
Dr-dyrane <halodyrane@gmail.com>
2023-08-01 05:27:35 -0700

    dyrane is awesome

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,20 +9,23 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	/* If the tree is NULL, it has height 0. */
-	if (tree == NULL)
-		return (0);
+	/* If the tree is NULL, return height as 0 */
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-	size_t left_height = 0, right_height = 0;
+		/* Recursively calculate the height of the left subtree */
+		if (tree->left)
+			left_height = 1 + binary_tree_height(tree->left);
 
-	/* Recursively calculate the height of the left subtree. */
-	if (tree->left)
-		left_height = 1 + binary_tree_height(tree->left);
+		/* Recursively calculate the height of the right subtree */
+		if (tree->right)
+			right_height = 1 + binary_tree_height(tree->right);
 
-	/* Recursively calculate the height of the right subtree. */
-	if (tree->right)
-		right_height = 1 + binary_tree_height(tree->right);
+		/* Return the maximum height between the left and right subtrees */
+		return (left_height > right_height ? left_height : right_height);
+	}
 
-	/* Return the maximum height between the left and right subtrees. */
-	return (left_height > right_height ? left_height : right_height);
+	/* If the tree is NULL, return 0 as specified in the requirements. */
+	return (0);
 }
