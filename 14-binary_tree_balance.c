8be5faf7c1a20c28df6fb6cf9edaa52b3f90ea36
Dr-dyrane <halodyrane@gmail.com>
2023-08-01 05:34:27 -0700

    dyrane is awesome

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,40 +2,24 @@
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree.
+ * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
- * Return: The balance factor of the tree.
+ * Return: If tree is NULL, the function returns 0. Otherwise, it returns the
+ *        balance factor of the binary tree.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	/* Check if the tree is empty or NULL (base case for recursion). */
-	if (tree == NULL)
-		return (0);
+	/* If the tree is NULL, return balance factor as 0 */
+	if (tree)
+	{
+		/* Calculate the height of the left and right subtrees */
+		int left_height = binary_tree_height(tree->left);
+		int right_height = binary_tree_height(tree->right);
 
-	/* Calculate the height of the left and right subtrees. */
-	int left_height = binary_tree_height(tree->left);
-	int right_height = binary_tree_height(tree->right);
+		/* Return the balance factor (difference in heights) */
+		return (left_height - right_height);
+	}
 
-	/* Calculate the balance factor (difference in heights). */
-	return (left_height - right_height);
-}
-
-/**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree.
- *
- * Return: The height of the tree.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	/* Check if the tree is empty or NULL (base case for recursion). */
-	if (tree == NULL)
-		return (0);
-
-	/* Calculate the height of the left and right subtrees. */
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	/* Return the maximum height between the left and right subtrees plus 1. */
-	return (1 + (left_height > right_height ? left_height : right_height));
+	/* If the tree is NULL, return 0 as specified in the requirements. */
+	return (0);
 }
