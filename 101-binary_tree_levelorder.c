6e6381cd6c15618ec98c2e0c202d2ad724c078ba
Dr-dyrane <halodyrane@gmail.com>
2023-08-01 06:43:00 -0700

    dyrane is awesome

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
+ *binary_tree_height - Measures the height of a binary tree.
+ *@tree: A pointer to the root node of the tree to measure the height.
  *
- * Return: If tree is NULL, the function returns 0. Otherwise, it returns
- *        the height of the binary tree.
+ *Return: If tree is NULL, the function returns 0. Otherwise, it returns
+ *       the height of the binary tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -16,17 +16,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		/*Recursively calculate the height of the left subtree */
 		if (tree->left)
-			left_height = 1 + binary_tree_height(tree->left);
+			left_height = binary_tree_height(tree->left);
 
 		/*Recursively calculate the height of the right subtree */
 		if (tree->right)
-			right_height = 1 + binary_tree_height(tree->right);
+			right_height = binary_tree_height(tree->right);
 
 		/*Return the maximum height between the left and right subtrees */
-		return (left_height > right_height ? left_height : right_height);
+		return (left_height > right_height ? left_height + 1 : right_height + 1);
 	}
 
-	/*If the tree is NULL, return 0 as specified in the requirements */
+	/*If the tree is NULL, return 0 as specified in the requirements. */
 	return (0);
 }
 
@@ -39,12 +39,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 void print_at_level(const binary_tree_t *tree, void(*func)(int), int level)
 {
 	/*Base case: If tree is NULL, do nothing */
-	if (tree)
+	if (tree && func)
 	{
 		/*If we reach the desired level, call the function to print the node */
 		if (level == 1)
 			func(tree->n);
-		else if (level > 1)
+		else
 		{ /*Recursively call print_at_level for left and right subtrees */
 			print_at_level(tree->left, func, level - 1);
 			print_at_level(tree->right, func, level - 1);
@@ -59,14 +59,18 @@ void print_at_level(const binary_tree_t *tree, void(*func)(int), int level)
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void(*func)(int))
 {
-	/*Check if the tree or function is NULL (base case) */
-	if (tree == NULL || func == NULL)
-		return;
+	size_t height = 0, i = 1;
 
-	/*Calculate the height of the binary tree */
-	size_t height = binary_tree_height(tree);
+	if (tree && func)
+	{
+		/*Calculate the height of the binary tree */
+		height = binary_tree_height(tree);
 
-	/*Traverse each level and call print_at_level to print nodes */
-	for (int level = 1; level <= height; level++)
-		print_at_level(tree, func, level);
+		/*Traverse each level and call print_at_level to print nodes */
+		while (i <= height + 1)
+		{
+			print_at_level(tree, func, i);
+			i++;
+		}
+	}
 }
